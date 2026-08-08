3693dfe1d9021621e2211ee40462e2fe8f5061ab
Emmanuel Gautier <git@emmanuelgautier.fr>
2026-08-08 11:15:55 +0200

    feat: add warning primitives

--- packages/ui/src/components/alert.tsx
+++ packages/ui/src/components/alert.tsx
@@ -11,6 +11,8 @@ const alertVariants = cva(
         default: 'bg-card text-card-foreground',
         destructive:
           'text-destructive bg-card [&>svg]:text-current *:data-[slot=alert-description]:text-destructive',
+        warning:
+          'text-warning bg-card [&>svg]:text-current *:data-[slot=alert-description]:text-warning',
       },
     },
     defaultVariants: {
